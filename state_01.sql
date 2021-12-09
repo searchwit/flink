4429c8849c89391e43f102b79ffb0a18ac144165
searchwit <94544499+searchwit@users.noreply.github.com>
2021-12-09 15:08:29 -0500

    Update state_01.sql

--- state_01.sql
+++ state_01.sql
@@ -1,66 +1,64 @@
-REM INSERTING into CC_STATE
-SET DEFINE OFF;
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6376,null,'CA','Manitoba','MB');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6377,null,'CA','Yukon','YT');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6378,null,'CA','Alberta','AB');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6379,null,'CA','New Brunswick','NB');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6380,null,'CA','Quebec','QC');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6381,null,'CA','Saskatchewan','SK');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6382,null,'CA','Northwest Territories','NT');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6383,null,'CA','Nunavut','NU');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6384,null,'CA','British Columbia','BC');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6385,null,'CA','Ontario','ON');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6386,null,'CA','Newfoundland and Labrador','NF');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6387,null,'CA','Prince Edward Island','PE');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (6388,null,'CA','Nova Scotia','NS');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8975,null,'US','Missouri','MO');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8976,null,'US','Wisconsin','WI');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8977,null,'US','North Carolina','NC');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8978,null,'US','North Dakota','ND');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8979,null,'US','Utah','UT');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8980,null,'US','Colorado','CO');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8981,null,'US','Hawaii','HI');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8982,null,'US','Texas','TX');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8983,null,'US','Iowa','IA');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8984,null,'US','Florida','FL');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8985,null,'US','Alabama','AL|');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8986,null,'US','South Dakota','SD');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8987,null,'US','Idaho','ID');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8988,null,'US','Connecticut','CT');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8989,null,'US','West Virginia','WV');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8990,null,'US','New Mexico','NM');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8991,null,'US','New York','NY');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8992,null,'US','District of Columbia','DC');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8993,null,'US','California','CA');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8994,null,'US','Delaware','DE');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8995,null,'US','New Hampshire','NH');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8996,null,'US','Tennessee','TN');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8997,null,'US','Arkansas','AR');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8998,null,'US','Georgia','GA');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (8999,null,'US','Louisiana','LA');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9000,null,'US','Michigan','MI');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9001,null,'US','Minnesota','MN');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9002,null,'US','Ohio','OH');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9003,null,'US','South Carolina','SC');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9004,null,'US','Arizona','AZ');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9005,null,'US','Massachusetts','MA');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9006,null,'US','Pennsylvania','PA');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9007,null,'US','Alaska','AK|');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9008,null,'US','Virginia','VA');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9009,null,'US','Wyoming','WY');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9010,null,'US','Maryland','MD');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9011,null,'US','Vermont','VT');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9012,null,'US','Nevada','NV');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9013,null,'US','Illinois','IL');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9014,null,'US','Oregon','OR');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9015,null,'US','Rhode Island','RI');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9016,null,'US','Indiana','IN');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9017,null,'US','New Jersey','NJ');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9018,null,'US','Oklahoma','OK');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9019,null,'US','Maine','ME');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9020,null,'US','Nebraska','NE');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9021,null,'US','Kentucky','KY');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9022,null,'US','Montana','MT');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9023,null,'US','Mississippi','MS');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9024,null,'US','Kansas','KS');
-Insert into CC_STATE (STATE_ID,STATE_CODE,COUNTRY_CODE,STATE_NAME,STATE_ALIAS) values (9025,null,'US','Washington','WA');
+update cc_state set state_alias ='MB' where state_id =6376
+update cc_state set state_alias ='YT' where state_id =6377
+update cc_state set state_alias ='AB' where state_id =6378
+update cc_state set state_alias ='NB' where state_id =6379
+update cc_state set state_alias ='QC' where state_id =6380
+update cc_state set state_alias ='SK' where state_id =6381
+update cc_state set state_alias ='NT' where state_id =6382
+update cc_state set state_alias ='NU' where state_id =6383
+update cc_state set state_alias ='BC' where state_id =6384
+update cc_state set state_alias ='ON' where state_id =6385
+update cc_state set state_alias ='NF' where state_id =6386
+update cc_state set state_alias ='PE' where state_id =6387
+update cc_state set state_alias ='NS' where state_id =6388
+update cc_state set state_alias ='MO' where state_id =8975
+update cc_state set state_alias ='WI' where state_id =8976
+update cc_state set state_alias ='NC' where state_id =8977
+update cc_state set state_alias ='ND' where state_id =8978
+update cc_state set state_alias ='UT' where state_id =8979
+update cc_state set state_alias ='CO' where state_id =8980
+update cc_state set state_alias ='HI' where state_id =8981
+update cc_state set state_alias ='TX' where state_id =8982
+update cc_state set state_alias ='IA' where state_id =8983
+update cc_state set state_alias ='FL' where state_id =8984
+update cc_state set state_alias ='AL|' where state_id =8985
+update cc_state set state_alias ='SD' where state_id =8986
+update cc_state set state_alias ='ID' where state_id =8987
+update cc_state set state_alias ='CT' where state_id =8988
+update cc_state set state_alias ='WV' where state_id =8989
+update cc_state set state_alias ='NM' where state_id =8990
+update cc_state set state_alias ='NY' where state_id =8991
+update cc_state set state_alias ='DC' where state_id =8992
+update cc_state set state_alias ='CA' where state_id =8993
+update cc_state set state_alias ='DE' where state_id =8994
+update cc_state set state_alias ='NH' where state_id =8995
+update cc_state set state_alias ='TN' where state_id =8996
+update cc_state set state_alias ='AR' where state_id =8997
+update cc_state set state_alias ='GA' where state_id =8998
+update cc_state set state_alias ='LA' where state_id =8999
+update cc_state set state_alias ='MI' where state_id =9000
+update cc_state set state_alias ='MN' where state_id =9001
+update cc_state set state_alias ='OH' where state_id =9002
+update cc_state set state_alias ='SC' where state_id =9003
+update cc_state set state_alias ='AZ' where state_id =9004
+update cc_state set state_alias ='MA' where state_id =9005
+update cc_state set state_alias ='PA' where state_id =9006
+update cc_state set state_alias ='AK|' where state_id =9007
+update cc_state set state_alias ='VA' where state_id =9008
+update cc_state set state_alias ='WY' where state_id =9009
+update cc_state set state_alias ='MD' where state_id =9010
+update cc_state set state_alias ='VT' where state_id =9011
+update cc_state set state_alias ='NV' where state_id =9012
+update cc_state set state_alias ='IL' where state_id =9013
+update cc_state set state_alias ='OR' where state_id =9014
+update cc_state set state_alias ='RI' where state_id =9015
+update cc_state set state_alias ='IN' where state_id =9016
+update cc_state set state_alias ='NJ' where state_id =9017
+update cc_state set state_alias ='OK' where state_id =9018
+update cc_state set state_alias ='ME' where state_id =9019
+update cc_state set state_alias ='NE' where state_id =9020
+update cc_state set state_alias ='KY' where state_id =9021
+update cc_state set state_alias ='MT' where state_id =9022
+update cc_state set state_alias ='MS' where state_id =9023
+update cc_state set state_alias ='KS' where state_id =9024
+update cc_state set state_alias ='WA' where state_id =9025
